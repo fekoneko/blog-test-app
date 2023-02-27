38d5ba510443b56bcaaa2f8c751ccdecfda75413
fekoneko <55813967+fekoneko@users.noreply.github.com>
2023-02-27 04:24:49 +0300

    Revert "Merge branch 'master' of https://github.com/fekoneko/fekoneko-blog"
    This reverts commit 351af41943c35e76952bdf2b82af3b3eeeb54f5a, reversing
    changes made to 035851c7da6233b4b5fa3b3f3ccdd8c285e7929e.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  base: '',
+export default defineConfig(({ mode }) => {
+  // development
+  if (mode === 'development')
+    return {
+      plugins: [react()],
+      base: '',
+    };
+  // production (for GitHub Pages)
+  else
+    return {
+      plugins: [react()],
+      base: '/fekoneko-blog/',
+    };
 });
